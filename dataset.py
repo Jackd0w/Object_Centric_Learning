20371141c230dea6ee05f39ac1c87e43436781fc
Jackd0w <62934328+Jackd0w@users.noreply.github.com>
2023-01-27 17:17:13 +0300

    Add files via upload
    Added wandb for train loop

---
+++ dataset.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from matplotlib import pyplot as plt
+import numpy as np
+import seaborn as sns
+
+# Change figure aesthetics
+
+sns.set_context('talk', font_scale=1.2, rc={'lines.linewidth': 1.5})
+
+dataset_zip = np.load('dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz')
+
+print('Keys in the dataset:', dataset_zip.keys())
+imgs = dataset_zip['imgs']
+latents_values = dataset_zip['latents_values']
+latents_classes = dataset_zip['latents_classes']
+metadata = dataset_zip['metadata'][()]
+
+print('Metadata: \n', metadata)
+
+latents_sizes = metadata['latents_sizes']
+latents_bases = np.concatenate((latents_sizes[::-1].cumprod()[::-1][1:],
+                                np.array([1,])))
+
+def latent_to_index(latents):
+  return np.dot(latents, latents_bases).astype(int)
+
+
+def sample_latent(size=1):
+  samples = np.zeros((size, latents_sizes.size))
+  for lat_i, lat_size in enumerate(latents_sizes):
+    samples[:, lat_i] = np.random.randint(lat_size, size=size)
+
+  return samples
+
+
+def show_images_grid(imgs_, num_images=25):
+      ncols = int(np.ceil(num_images**0.5))
+  nrows = int(np.ceil(num_images / ncols))
+  _, axes = plt.subplots(ncols, nrows, figsize=(nrows * 3, ncols * 3))
+  axes = axes.flatten()
+
+  for ax_i, ax in enumerate(axes):
+    if ax_i < num_images:
+      ax.imshow(imgs_[ax_i], cmap='Greys_r',  interpolation='nearest')
+      ax.set_xticks([])
+      ax.set_yticks([])
+    else:
+      ax.axis('off')
+
+def show_density(imgs):
+  _, ax = plt.subplots()
+  ax.imshow(imgs.mean(axis=0), interpolation='nearest', cmap='Greys_r')
+  ax.grid('off')
+  ax.set_xticks([])
+  ax.set_yticks([])
